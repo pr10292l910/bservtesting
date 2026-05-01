1ba05c8eb4fd9d12060f574e75cc3f405fba19fa
pr10292l910 <pruebahojas500@gmail.com>
2026-05-01 17:51:29 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-# Iniciar playit en segundo plano (tunnel)
+# Servidor HTTP mínimo en puerto 10000 para que Render no mate el proceso
+python3 -c "
+import http.server, threading
+class H(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'BombSquad server running')
+    def log_message(self, *args): pass
+s = http.server.HTTPServer(('0.0.0.0', 10000), H)
+threading.Thread(target=s.serve_forever, daemon=True).start()
+" &
+
+# Iniciar playit en segundo plano
 ./playit &
 
 # Iniciar servidor BombSquad
